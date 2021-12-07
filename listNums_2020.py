ede54169ad2119771e9a20ec4be7ad61b46b92af
Benjamin P. Stager <82251659+benstager@users.noreply.github.com>
2021-12-07 14:32:31 -0600

    Add files via upload

---
+++ listNums_2020.py
@@ -0,0 +1,49 @@
+import random
+lst = []
+for i in range(100):
+    lst.append(random.randint(1,501))
+
+print("Original list:")
+for i in range(100):
+    print(lst[i])
+
+print()
+print()
+
+print("Sorted list:")
+lst.sort()
+for i in range(100):
+    print(lst[i])
+    
+print()
+print()
+
+print("Reversed sorted list:")
+lst.reverse()
+for i in range(100):
+    print(lst[i])
+print()
+print()
+
+print("Please ask for a number")
+num = int(input())
+
+while num not in lst:
+    print("Sorry your number isn't here, guess again")
+    num = int(input())
+count = 0
+
+    
+for i in range(100):
+    if num == lst[i]:
+        count = count + 1
+
+print("The number appeared", count, "time(s)")
+
+
+m = max(lst)
+mi = min(lst)
+
+print("Max:", m)
+print("Min:", mi)
+
